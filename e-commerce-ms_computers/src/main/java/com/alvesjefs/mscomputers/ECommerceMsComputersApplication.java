1cc89c4f8e8416d0e970dd9e25075deb01e89d20
Jefferson Alves <jeffersonalmeida16@outlook.com>
2021-01-10 22:27:54 -0300

    e-commerce-ms_computers

---
+++ e-commerce-ms_computers/src/main/java/com/alvesjefs/mscomputers/ECommerceMsComputersApplication.java
@@ -0,0 +1,13 @@
+package com.alvesjefs.mscomputers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMsComputersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMsComputersApplication.class, args);
+	}
+
+}
---
+++ e-commerce-ms_computers/src/test/java/com/alvesjefs/mscomputers/ECommerceMsComputersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvesjefs.mscomputers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMsComputersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
